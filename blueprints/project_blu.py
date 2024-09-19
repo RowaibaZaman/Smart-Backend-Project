6df3d7634f8b686a3c2fb687119486af762d584c
RowaibaZaman <77125611+RowaibaZaman@users.noreply.github.com>
2024-09-19 15:04:24 +0500

    .

--- blueprints/project_blu.py
+++ blueprints/project_blu.py
@@ -7,6 +7,8 @@ from bl.project_bl import Project_BL
 
 project_bp = Blueprint('project', __name__ )
 
+#add project
+
 @project_bp.route('/add_project', methods =['POST'])
 @use_args({
     "project_name": fields.Str(required = True),
@@ -20,6 +22,9 @@ def add_project(args):
  
     result = Project_BL.add_project_bl(args)
     return jsonify(result), 201
+
+
+#get projects
 
 @project_bp.route('/get_project', methods =['GET'])
 def get_projecct():
@@ -37,26 +42,33 @@ def p_with_t(args):
     result = Project_BL.project_with_task_bl(project_id)
     return jsonify(result)
 
+
+#update project
+
 @project_bp.route('/update_project', methods=['PUT'])
 @use_args({
-    "project_id": fields.Int(required=True),         # Get project_id from query params
-    "project_name": fields.Str(required=False),      # Get update fields from JSON body
+    "project_id": fields.Int(required=True),         
+    "project_name": fields.Str(required=False),      
     "start_date": fields.Str(required=False),
     "end_date": fields.Str(required=False),
     "description": fields.Str(required=False)
-}, location='json')  # This will allow the args to come from both query and json
+}, location='json')  
+
 def update_project(args):
     project_id = args.get('project_id')  # Extract project_id from query parameters
     update_data = {k: v for k, v in args.items() if v is not None and k != 'project_id'}  # Extract fields to update
     
-    # Call the business logic with project_id and update_data
+    # Call the bl to update
     result = Project_BL.update_project_bl(project_id, update_data)
     return jsonify(result)
 
 
+
+#Pagination
+
 @project_bp.route('/get_paginated_projects', methods=['GET'])
 @use_args({
-    "limit": fields.Int(required=False, missing=2),  # Default limit is 10
+    "limit": fields.Int(required=False, missing=2),  # Default limit is 2
     "page": fields.Int(required=False, missing=1)     # Default page is 1
 }, location='query')
 def get_paginated_projects(args):
